df94eaf6008867807fabf0e187e465940f0379fa
Brian-15 <bcirieco@gmail.com>
2022-02-10 18:58:35 -0800

    implemented sameFrequency function

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+/**
+ * Given two positive integers, find out if the two numbers have the same frequency of digits.
+ */
+function sameFrequency(num1, num2) {
+  const freq1 = getFrequency(String(num1));
+
+  for (const digit of String(num2)) {
+    if (!freq1[digit] || freq1[digit] === 0) return false;
+    freq1[digit]--;
+  }
+
+  return true;
 }
+
+function getFrequency(str) {
+  const freq = {};
+  for (const char of str) {
+    freq[char] = freq[char] ? freq[char] + 1 : 1;
+  }
+  return freq;
+}
+
+module.exports = sameFrequency;
