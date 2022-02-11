063c67aab0eec916ac6eea25ae28d8a23f684a42
Brian-15 <bcirieco@gmail.com>
2022-02-10 18:30:36 -0800

    implemented constructNote

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,37 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+/** Accepts two strings: a messsage, and some letters
+ *  This function should return true if the message can be built with the letters
+ *  you are given; otherwise, it should return false.
+ * 
+ * assume there are only lowercase letters and no space or special characters 
+ * in both the message and the letters.
+ */
+
+function constructNote(message, letters) {
+  if (message.length === 0) return true;
+  if (message.length > letters.length) return false;
+  const letterFreq = getCharFreq(letters);
+
+  for (const letter of message) {
+    if (!letterFreq[letter] || letterFreq[letter] === 0) return false;
+    letterFreq[letter] -= 1;
+  }
+
+  return true;
 }
+
+function getCharFreq(str) {
+  const freq = {};
+
+  for (let char of str) {
+    if (freq[char]) {
+      freq[char]++;
+    } else {
+      freq[char] = 1;
+    }
+  }
+
+  return freq;
+}
+
+module.exports = constructNote;
