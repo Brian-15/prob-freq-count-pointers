4a953f18e63167a29a45f7cfb4e5fcd1aab3b259
Brian-15 <bcirieco@gmail.com>
2022-02-11 14:06:09 -0800

    implemented isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+/**
+ * takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string.
+ * In other words, the function should check whether the characters in the first string appear somewhere in the second string,
+ * without their order changing.
+ */
+function isSubsequence(str1, str2) {
+  if (str1.length > str2.length) return false;
+
+  let idx1 = 0, count = 0;
+
+  for (const letter of str2) {
+    if (str1[idx1] === letter) {
+      count++;
+      idx1++;
+    }
+  }
+  return count === str1.length;
 }
+
+module.exports = isSubsequence;
