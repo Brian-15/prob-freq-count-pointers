dcd7c3920c07b9a2d2beb378bed0f119cb7efeb1
Brian-15 <bcirieco@gmail.com>
2022-02-10 18:50:19 -0800

    implemented twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+/**
+ * accepts two arrays of varying lengths.The first array consists of keys and the second one consists of values.
+ * Your function should return an object created from the keys and values.
+ * If there are not enough values, the rest of keys should have a value of null.
+ * If there not enough keys, just ignore the rest of values.
+ */
+function twoArrayObject(keys, vals) {
+  const obj = {};
+  let valIdx = 0;
+
+  for (const key of keys) {
+    obj[key] = vals[valIdx] ? vals[valIdx] : null;
+    valIdx++;
+  }
+
+  return obj;
 }
+
+module.exports = twoArrayObject;
