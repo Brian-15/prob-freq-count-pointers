eba3d63dc9a9f092d0b5ad931c439399d4009c56
Brian-15 <bcirieco@gmail.com>
2022-02-11 13:49:49 -0800

    implemented separatePositive function

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,37 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+/**
+ * accepts an array of non-zero integers.
+ * Separate the positive integers to the left and the negative integers to the right.
+ * The positive numbers and negative numbers need not be in sorted order.
+ * The problem should be done in place (in other words, do not build a copy of the input array).
+ */
+function separatePositive(arr) {
+  if (!arr.length) return [];
+  let [left, right] = [0, arr.length - 1];
+
+  console.log(arr);
+
+  while (left < right) {
+    if (arr[left] > 0 && arr[right] < 0) {
+      left++;
+      right--;
+      continue;
+    } else if (arr[left] < 0 && arr[right] > 0) {
+      // swap values
+      const temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+      right--;
+    } else if (arr[left] < 0) {
+      right--;
+    } else if (arr[right] > 0) {
+      left++;
+    }
+  }
+
+  console.log(arr)
+  return arr;
 }
+
+module.exports = separatePositive;
